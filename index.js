df6a81b246b0b1a97fe9cec1c96e9581ceb60e30
ckorir <chrisruto.k@gmail.com>
2023-09-15 13:01:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+//returns an array of thank you messages for each name provided to the function
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names){
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        debugger;
+    }
+    return thankYouMessages;
+}
+
+//invokes console.log once for each number, counting down from the number provided to zero, logs each number when counting down, starting from the number provided
+function countDown(number){
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
